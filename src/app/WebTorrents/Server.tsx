eaf46e1f3b38463bcdba4d0e21f6a3c74f613547
Paavan Bhavsar <paavan@kensho.com>
2020-01-16 23:17:02 -0500

    Add logging for peer ids

--- src/app/WebTorrents/Server.tsx
+++ src/app/WebTorrents/Server.tsx
@@ -3,36 +3,36 @@ import * as WebTorrent from 'webtorrent'
 
 import useStableValue from '../../hooks/useStableValue'
 
-const SEED = '93ab6361-37fe-4d6a-b0ee-23c72cc938e2'
+const SEED = '6c0d50e0-56c9-4b43-bccf-77f346dd0e04'
 
-const TRACKERS = [
-  'wss://hub.bugout.link',
-  'wss://tracker.openwebtorrent.com',
-  'wss://tracker.btorrent.xyz',
-]
+const TRACKERS = ['wss://tracker.openwebtorrent.com', 'wss://tracker.btorrent.xyz']
+
+function log(...values: any[]) {
+  console.debug('[WT] ', ...values)
+}
 
 export default function Server(): JSX.Element {
   // connect to server using its public address
   const torrentClient = (() => {
-    console.log('CLIENT')
     const peersSeen = new Set<string>()
     const client = new WebTorrent({
       tracker: {
         getAnnounceOpts: () => ({numwant: 50}),
       },
     })
+    log('Initialized client with peer id: ', client.peerId)
 
     const torrent = client.seed(Buffer.from(SEED), {name: SEED, announce: TRACKERS})
     torrent.on('wire', (wire, address) => {
       if (!peersSeen.has(wire.peerId)) {
-        console.debug('Found new peer: ', wire.peerId)
-      } else {
-        console.debug('Reconnected to peer: ', wire.peerId)
+        log('Found new peer: ', wire.peerId)
         peersSeen.add(wire.peerId)
+      } else {
+        log('Reconnected to peer: ', wire.peerId)
       }
-      console.debug('Wire: ', wire)
+      log('Wire: ', wire)
       wire.on('handshake', (infoHash, peerId, extensions) => {
-        console.debug('Handshake: ', infoHash, peerId, extensions)
+        log('Handshake: ', infoHash, peerId, extensions)
       })
     })
 
