85b8458938519e53ee1d5f627485f5090d99e1b3
Paavan Bhavsar <paavan@kensho.com>
2020-01-18 18:39:02 -0500

    Refactored extension to be able to take arguments

--- src/app/WebTorrents/Server.tsx
+++ src/app/WebTorrents/Server.tsx
@@ -4,7 +4,11 @@ import {Wire} from 'bittorrent-protocol'
 
 import log from '../../lib/log'
 
-import CommunicationExtension from './extension'
+import swarmCommunicationExtension, {SwarmCommExtension} from './extension'
+
+interface SwarmExtendedWire extends Wire {
+  swarm_comm_ext: SwarmCommExtension
+}
 
 const SEED = '6c0d50e0-56c9-4b43-bccf-77f346dd0e04'
 
@@ -12,7 +16,7 @@ const TRACKERS = ['wss://tracker.openwebtorrent.com', 'wss://tracker.btorrent.xy
 
 export default function Server(): JSX.Element {
   const clientRef = React.useRef<WebTorrent.Instance | null>(null)
-  const [conns, setConns] = React.useState<Record<string, CommunicationExtension>>({})
+  const [conns, setConns] = React.useState<Record<string, SwarmCommExtension>>({})
   const [text, setText] = React.useState('')
 
   React.useEffect(() => {
@@ -25,11 +29,17 @@ export default function Server(): JSX.Element {
     log('Initialized client with peer id: ', client.peerId)
 
     const torrent = client.seed(Buffer.from(SEED), {name: SEED, announce: TRACKERS})
-    torrent.on('wire', (wire, address) => {
-      wire.use(CommunicationExtension)
-      const {peerId} = wire
+    torrent.on('wire', (wire: Wire) => {
+      const extension = swarmCommunicationExtension()
+      wire.use(extension)
+      // eslint-disable-next-line no-param-reassign
+      const extWire = wire as SwarmExtendedWire
+      const {peerId} = extWire
 
-      setConns(prevWires => ({...prevWires, [peerId]: wire.test_ext as CommunicationExtension}))
+      setConns(prevWires => ({
+        ...prevWires,
+        [peerId]: extWire.swarm_comm_ext,
+      }))
 
       wire.setKeepAlive(true)
 
--- src/app/WebTorrents/extension.tsx
+++ src/app/WebTorrents/extension.tsx
@@ -5,56 +5,71 @@ import {Wire} from 'bittorrent-protocol'
 
 import log from '../../lib/log'
 
+// Extension name
+const EXT = 'swarm_comm_ext'
+
 type Extensions = {dht: boolean; extended: boolean}
 
 type Handshake = {
   m?: {
-    test_ext?: number
+    swarm_comm_ext?: number
   }
 }
 
-// Extension name
-const EXT = 'test_ext'
-
-function checkExtensionCompatibility(handshake: Handshake): void {
-  if (!handshake.m || !handshake.m.test_ext)
+function assertExtensionCompatibility(handshake: Handshake): void {
+  if (!handshake.m || !handshake.m.swarm_comm_ext)
     throw Error(`Peer does not support the extension '${EXT}'`)
   log('Compatible peer found.')
 }
 
-export default class CommunicationExtension extends EventEmitter {
-  wire: Wire
-
-  name: string
-
-  constructor(wire: Wire) {
-    super()
-    this.wire = wire
-    this.name = EXT
-  }
-
-  public onHandshake = (infoHash: string, peerId: string, extensions: Extensions): void => {
-    log('onHandshake with: ', infoHash, peerId, extensions)
-  }
-
-  public onExtendedHandshake = (handshake: Handshake): void => {
-    log('onExtendedHandshake with: ', handshake)
-    checkExtensionCompatibility(handshake)
-  }
-
-  public onMessage = (buffer: Buffer): void => {
-    if (!Buffer.isBuffer(buffer)) throw Error('Received non-buffer response.')
-
-    const data: unknown = bencode.decode(buffer)
-    log('Message Received: ', new TextDecoder('utf-8').decode(data.message))
-  }
-
-  public send(data: unknown): void {
-    log('Sending: ', data)
-    this.wire.extended(EXT, data)
-    log('Sent.')
-  }
+export interface SwarmCommExtension {
+  send: (data: unknown) => void
 }
 
-// bittorrent-protocol errors if name is not on the prototype
-CommunicationExtension.prototype.name = EXT
+/*
+ * Responsible for:
+ *  * Establish communication channels to peers
+ *  * Signing and verifying messages
+ *  * Keeping track of 'live' peers
+ *  * Data-agnostic (after handshake)
+ */
+export default function swarmCommunicationExtension() {
+  class CommunicationExtension extends EventEmitter implements SwarmCommExtension {
+    wire: Wire
+
+    name: 'swarm_comm_ext'
+
+    constructor(wire: Wire) {
+      super()
+      this.wire = wire
+      this.name = EXT
+    }
+
+    public onHandshake = (infoHash: string, peerId: string, extensions: Extensions): void => {
+      log('onHandshake with: ', infoHash, peerId, extensions)
+    }
+
+    public onExtendedHandshake = (handshake: Handshake): void => {
+      log('onExtendedHandshake with: ', handshake)
+      assertExtensionCompatibility(handshake)
+    }
+
+    public onMessage = (buffer: Buffer): void => {
+      if (!Buffer.isBuffer(buffer)) throw Error('Received non-buffer response.')
+
+      const data: unknown = bencode.decode(buffer)
+      log('Message Received: ', new TextDecoder('utf-8').decode(data.message))
+    }
+
+    public send(data: unknown): void {
+      log('Sending: ', data)
+      this.wire.extended(EXT, data)
+      log('Sent.')
+    }
+  }
+
+  // bittorrent-protocol errors if name is not on the prototype
+  CommunicationExtension.prototype.name = EXT
+
+  return CommunicationExtension
+}
