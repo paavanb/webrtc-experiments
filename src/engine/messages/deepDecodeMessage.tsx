67c8165d42356e47cf36ca1e4cfbd07962ce4d0b
Paavan Bhavsar <paavanb@gmail.com>
2020-02-08 17:06:39 -0500

    Fix deepDecodeMessage logic

--- src/engine/messages/deepDecodeMessage.tsx
+++ src/engine/messages/deepDecodeMessage.tsx
@@ -1,4 +1,22 @@
 const textDecoder = new TextDecoder('utf-8')
+
+function deepDecodeValue(value: unknown): unknown {
+  let decodedValue = value
+  if (value instanceof Uint8Array) {
+    decodedValue = textDecoder.decode(value)
+  } else if (Array.isArray(value)) {
+    decodedValue = value.map(deepDecodeValue)
+  } else if (typeof value === 'object' && value !== null) {
+    const obj = value as Record<string, unknown>
+    decodedValue = Object.keys(obj).reduce(
+      (decodedObj, key) => Object.assign(decodedObj, {[key]: deepDecodeValue(obj[key])}),
+      {}
+    )
+  }
+
+  return decodedValue
+}
+
 /**
  * Deep-convert all Uint8[] fields to strings.
  * Currently, our messages do not store any binary data, so we can
@@ -6,16 +24,5 @@ const textDecoder = new TextDecoder('utf-8')
  * Assumes data is a JSON object.
  */
 export default function deepDecodeMessage(data: Record<string, unknown>): object {
-  return Object.keys(data).reduce((decodedObj, key) => {
-    const value = data[key]
-    let decodedValue = value
-    if (value instanceof Uint8Array) {
-      decodedValue = textDecoder.decode(value)
-    } else if (Array.isArray(value)) {
-      decodedValue = value.map(deepDecodeMessage)
-    } else if (typeof value === 'object' && value !== null) {
-      decodedValue = deepDecodeMessage(value as Record<string, unknown>)
-    }
-    return Object.assign(decodedObj, {[key]: decodedValue})
-  }, {} as object)
+  return deepDecodeValue(data) as object
 }
