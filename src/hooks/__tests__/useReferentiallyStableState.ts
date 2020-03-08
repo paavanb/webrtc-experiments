8f163db8c4aa218eca76a3c5177fc3725c0a1304
Paavan Bhavsar <paavanb@gmail.com>
2020-03-08 19:48:40 -0400

    Add implementation for useReferentiallyStableState and tests

--- src/hooks/__tests__/useReferentiallyStableState.ts
+++ src/hooks/__tests__/useReferentiallyStableState.ts
@@ -4,70 +4,18 @@ import useReferentiallyStableState from '../useReferentiallyStableState'
 
 jest.useFakeTimers()
 
-describe('#useReferentiallyStableState', () => {
-  it('initializes to the passed-in value', () => {
-    const obj = {a: 1, b: 2}
-    const {result} = renderHook(() => useReferentiallyStableState(obj))
-    const [value] = result.current
-
-    expect(value).toBe(obj)
-  })
-
-  it('correctly invalidates numbers', () => {
-    const obj = {value: 1}
-    const {result} = renderHook(v => useReferentiallyStableState(v), {
-      initialProps: obj,
-    })
-    const [value, setValue] = result.current
-
-    expect(value).toBe(obj)
-
-    const newObj = {value: 2}
-
-    act(() => {
-      setValue(newObj)
-      jest.advanceTimersByTime(100)
-    })
-
-    const [newValue] = result.current
-
-    expect(newValue).toBe(newObj)
-    expect(newValue.value).not.toBe(obj.value)
-  })
-
-  it('correctly invalidates booleans', () => {
-    const obj = {value: false}
-    const {result} = renderHook(v => useReferentiallyStableState(v), {
-      initialProps: obj,
-    })
-    const [value, setValue] = result.current
-
-    expect(value).toBe(obj)
-
-    const newObj = {value: true}
-
-    act(() => {
-      setValue(newObj)
-      jest.advanceTimersByTime(100)
-    })
-
-    const [newValue] = result.current
-
-    expect(newValue.value).toBe(newObj.value)
-    expect(newValue.value).not.toBe(obj.value)
-  })
-
-  describe('strings', () => {
-    it('maintains equality if the string is unchanged', () => {
-      const obj = {value: 'hello'}
-      const {result} = renderHook(v => useReferentiallyStableState(v), {
+function itHandlesPrimitiveTypes<T>(typeName: string, initialValue: T, updatedValue: T): void {
+  describe(typeName, () => {
+    it('maintains equality if the value is unchanged', () => {
+      const obj = {value: initialValue}
+      const {result} = renderHook(useReferentiallyStableState, {
         initialProps: obj,
       })
       const [value, setValue] = result.current
 
       expect(value).toBe(obj)
 
-      const newObj = {value: 'hello'}
+      const newObj = {value: initialValue}
 
       act(() => {
         setValue(newObj)
@@ -79,16 +27,16 @@ describe('#useReferentiallyStableState', () => {
       expect(newValue.value).toEqual(newObj.value)
     })
 
-    it('correctly invalidates strings', () => {
-      const obj = {value: 'hello'}
-      const {result} = renderHook(v => useReferentiallyStableState(v), {
+    it('correctly invalidates the value if changed', () => {
+      const obj = {value: initialValue}
+      const {result} = renderHook(useReferentiallyStableState, {
         initialProps: obj,
       })
       const [value, setValue] = result.current
 
       expect(value).toBe(obj)
 
-      const newObj = {value: 'goodbye'}
+      const newObj = {value: updatedValue}
 
       act(() => {
         setValue(newObj)
@@ -97,8 +45,104 @@ describe('#useReferentiallyStableState', () => {
 
       const [newValue] = result.current
 
-      expect(newValue).toBe(newObj)
+      expect(newValue).toEqual(newObj)
       expect(newValue.value).not.toBe(obj.value)
+    })
+  })
+}
+
+describe('#useReferentiallyStableState', () => {
+  it('initializes to the passed-in value', () => {
+    const obj = {a: 1, b: 2}
+    const {result} = renderHook(() => useReferentiallyStableState(obj))
+    const [value] = result.current
+
+    expect(value).toBe(obj)
+  })
+
+  itHandlesPrimitiveTypes('numbers', 1, 2)
+  itHandlesPrimitiveTypes('booleans', true, false)
+  itHandlesPrimitiveTypes('strings', 'hello', 'goodbye')
+
+  describe('objects', () => {
+    it('maintains referential equality if the new updated value is equivalent to the old value.', () => {
+      const obj = {value: {a: 1}}
+      const {result} = renderHook(useReferentiallyStableState, {initialProps: obj})
+      const [value, setValue] = result.current
+
+      expect(value).toBe(obj)
+
+      const newObj = {value: {a: 1}}
+
+      act(() => {
+        setValue(newObj)
+        jest.advanceTimersByTime(100)
+      })
+
+      const [newValue] = result.current
+
+      // The entire object should maintain the same reference since nothing changed
+      expect(newValue).toBe(obj)
+    })
+
+    it('maintains referential equality if the new updated value is equivalent to the old value.', () => {
+      const obj = {value: {a: 1}, b: 2}
+      const {result} = renderHook(useReferentiallyStableState, {initialProps: obj})
+      const [value, setValue] = result.current
+
+      expect(value).toBe(obj)
+
+      const newObj = {value: {a: 1}, b: 3}
+
+      act(() => {
+        setValue(newObj)
+        jest.advanceTimersByTime(100)
+      })
+
+      const [newValue] = result.current
+
+      // Though obj.b changed, obj.value should be equivalent
+      expect(newValue.value).toBe(obj.value)
+      expect(newValue.b).toBe(3)
+    })
+
+    it('recursively maintains referential equality if a new updated sub-value is equivalent to the old sub-value.', () => {
+      const obj = {
+        value: {
+          a: 1,
+          b: 2,
+          c: {
+            d: 'hello',
+          },
+        },
+        e: true,
+      }
+      const {result} = renderHook(useReferentiallyStableState, {initialProps: obj})
+      const [value, setValue] = result.current
+
+      expect(value).toBe(obj)
+
+      const newObj = {
+        value: {
+          a: 3,
+          b: 2,
+          c: {
+            d: 'hello',
+          },
+        },
+        e: true,
+      }
+
+      act(() => {
+        setValue(newObj)
+        jest.advanceTimersByTime(100)
+      })
+
+      const [newValue] = result.current
+
+      expect(newValue).toEqual(newObj)
+      expect(newValue.value).not.toBe(obj.value)
+      expect(newValue.value.c).toBe(obj.value.c)
     })
   })
 })
--- src/hooks/useReferentiallyStableState.tsx
+++ src/hooks/useReferentiallyStableState.tsx
@@ -1,9 +1,51 @@
 import {useState, useCallback, Dispatch, SetStateAction} from 'react'
 
+function refStableReplace<T extends object>(obj: T, nextObj: T): T {
+  // Recursively replace all subkeys
+  const replaced = Object.keys(nextObj).reduce((replacedObj, key) => {
+    // @ts-ignore
+    const value = obj[key]
+    // @ts-ignore
+    const nextValue = nextObj[key]
+    const valueType = typeof nextValue
+    let replacedValue
+    switch (valueType) {
+      case 'string':
+      case 'number':
+      case 'bigint':
+      case 'boolean':
+        replacedValue = nextValue
+        break
+      case 'object':
+        if (value !== undefined && typeof value === 'object') {
+          replacedValue = refStableReplace(value, nextValue)
+        } else {
+          replacedValue = nextValue
+        }
+        break
+      default:
+        throw Error(`Invalid object provided, containing item of type '${valueType}'`)
+    }
+
+    return Object.assign(replacedObj, {[key]: replacedValue})
+  }, {} as T)
+
+  // Special case: the new replaced object is identical to the original object across all keys.
+  // Maintain ref equality by simply returning the original object
+  const allKeys = Array.from(new Set([...Object.keys(replaced), ...Object.keys(obj)]))
+  // @ts-ignore
+  if (allKeys.map(key => replaced[key] === obj[key]).every(Boolean)) {
+    return obj
+  }
+
+  return replaced
+}
+
 /**
  * Manage referentially-stable state. That is, on update the new value is
- * deep-compared with the previous value, such that a path `p` is equal
- * (by Object.is) to the path `p` in the new state iff the values are deep-equal.
+ * deep-compared with the previous value rather than replaced blindly, such
+ * that a path `p` is equal (by Object.is) to the path `p` in the new state
+ * iff the values are deep-equal.
  *
  * @example
  * ```
@@ -28,12 +70,13 @@ export default function useReferentiallyStableState<T extends object>(
 ): [T, Dispatch<SetStateAction<T>>] {
   const [value, setValue] = useState(initial)
 
-  //const deepSetValue = useCallback(
-    //(arg: T | ((prev: T) => T)) => {
-      //const newValue: T = typeof arg === 'function' ? arg(value) : arg
-    //},
-    //[value]
-  //)
+  const deepSetValue = useCallback(
+    (arg: T | ((prev: T) => T)) => {
+      const newValue: T = typeof arg === 'function' ? (arg as Function)(value) : arg
+      setValue(refStableReplace(value, newValue))
+    },
+    [value]
+  )
 
-  return [value, setValue]
+  return [value, deepSetValue]
 }
