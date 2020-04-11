378ecf179915a9e8e26e2af91002480091805784
Paavan Bhavsar <paavanb@gmail.com>
2020-04-11 15:04:19 -0400

    Allow dragging a card vertically

--- src/app/Game/PlayerHand.tsx
+++ src/app/Game/PlayerHand.tsx
@@ -39,10 +39,17 @@ export default function PlayerHand(props: PlayerHandProps): JSX.Element {
   const {cards} = props
   const containerRef = useRef<HTMLDivElement | null>(null)
   const containerWidth = useRef(CARD_WIDTH)
+  const isDraggingCard = useRef(false)
 
   const totalCardsWidth = (cards.length - 1) * CARD_WIDTH
   const leftCardBound = Math.min(0, -(totalCardsWidth - containerWidth.current / 2 + CARD_WIDTH / 2)) // eslint-disable-line prettier/prettier
   const rightCardBound = containerWidth.current / 2 - CARD_WIDTH / 2
+
+  const getCardDistanceFromCenter = useCallback((cardIndex: number, handX: number): number => {
+    const cardPos = cardIndex * CARD_WIDTH + handX
+    const distFromCenter = Math.abs(cardPos - (containerWidth.current / 2 - CARD_WIDTH / 2))
+    return distFromCenter
+  }, [])
 
   // Springs to indicate the current card, i.e., appearing raised slightly
   const [currentCardStyleProps, setCurrentCardStyleSprings] = useSprings(cards.length, () => ({
@@ -62,8 +69,7 @@ export default function PlayerHand(props: PlayerHandProps): JSX.Element {
       // Make cards appear "selected" depending on their distance from the center
       setCurrentCardStyleSprings(i => {
         // Center of the card
-        const cardPos = i * CARD_WIDTH + x
-        const distFromCenter = Math.abs(cardPos - (containerWidth.current / 2 - CARD_WIDTH / 2))
+        const distFromCenter = getCardDistanceFromCenter(i, x)
         const THRESHOLD = CARD_WIDTH / 2
         if (distFromCenter > THRESHOLD) {
           return {
@@ -107,82 +113,112 @@ export default function PlayerHand(props: PlayerHandProps): JSX.Element {
   )
 
   const cardDragEvents = useDrag(
-    ({args: [cardIndex], movement: [, dy], down}) => {
+    ({args: [cardIndex], movement: [, dy], vxvy: [, vy], down}) => {
       setCardSprings(i => {
-        const isDraggingCard = down && i === cardIndex
+        if (i !== cardIndex) return {}
+
+        isDraggingCard.current = down
+        const THRESHOLD = 10
+        const distFromCenter = getCardDistanceFromCenter(i, dragXProps.x.getValue())
+
+        // Only allow swiping a card if it is sufficiently close to being the selected card
+        if (distFromCenter > THRESHOLD) {
+          return {}
+        }
+
+        if (isDraggingCard.current) {
+          return {
+            to: {
+              // Drag selected card
+              y: isDraggingCard.current ? dy : 0,
+            },
+            immediate: isDraggingCard.current,
+          }
+        }
+
+        // TODO Support "throwing" the card
         return {
           to: {
-            // Drag selected card
-            y: isDraggingCard ? dy : 0,
+            y: 0,
           },
-          immediate: isDraggingCard,
+          immediate: false,
         }
       })
     },
     {
       bounds: {top: -100, bottom: 100},
+      axis: 'y',
     }
   )
 
-  const handDragEvents = useDrag(({vxvy: [vx], delta: [dx], down}) => {
-    const isDragging = down
-    const pos = dragXProps.x.getValue()
-    if (isDragging) {
-      setDragXSpring({
-        to: {
-          x: dragXProps.x.getValue() + dx,
-        },
-        config: {
-          velocity: 0,
-        },
-        immediate: true,
-      })
-    } else if (Math.abs(vx) < CARD_VELOCITY_SNAP_THRESHOLD) {
-      setDragXSpring({
-        to: {
-          x: clamp(leftCardBound, rightCardBound, getSnapPos(pos)),
-        },
-        immediate: false,
-        config: {
-          velocity: vx * FPS,
-          ...config.default,
-        },
-      })
-    } else {
-      // User has stopped dragging, determine a target position mimicking inertia
-      // and then snap to the closest card position.
-      // Calculate target displacement using exponential decay function
-      const lambda = 0.3
-      // react-spring uses px/s, while useDrag uses px/frame @60fps
-      const velocity = Math.sign(vx) * Math.max(Math.abs(vx) * FPS, 10) // Set lower bound to prevent singularity and provide "snapping" feel when the user lets go at low velocities
-      // Exponential decay technically takes infinite time. The only way to solve for time
-      // is to bound it by a final velocity > 0
-      // Base eqs:
-      // vf = vi * e^(-lambda * t)
-      // t = -1 / lambda * ln (vf / vi)
-      const targetVelocity = 1 * Math.sign(velocity)
-      const displacement = velocity / lambda
-      const target = pos + displacement
-      const duration = -Math.log(targetVelocity / velocity) / lambda
-      // Exponential decay function found by solving for t and normalizing to f: [0, 1] => [0, 1]
-      // TODO Unsure why I have to multiply by 10, but it makes the initial velocity match up properly
-      const easing = (t: number): number =>
-        (-velocity / (lambda * displacement)) * (Math.exp(-lambda * (t * 10) * duration) - 1)
+  const handDragEvents = useDrag(
+    ({vxvy: [vx], delta: [dx], down, cancel}) => {
+      // Disable hand dragging if the player is dragging a card
+      if (isDraggingCard.current) {
+        if (cancel) cancel()
+        return
+      }
 
-      setDragXSpring({
-        to: {
-          x: clamp(leftCardBound, rightCardBound, getSnapPos(target)),
-        },
-        immediate: false,
-        config: {
-          velocity,
-          // Convert to ms
-          duration: duration * 1000,
-          easing,
-        },
-      })
-    }
-  })
+      const isDragging = down
+      const pos = dragXProps.x.getValue()
+      if (isDragging) {
+        setDragXSpring({
+          to: {
+            x: dragXProps.x.getValue() + dx,
+          },
+          config: {
+            velocity: 0,
+          },
+          immediate: true,
+        })
+      } else if (Math.abs(vx) < CARD_VELOCITY_SNAP_THRESHOLD) {
+        setDragXSpring({
+          to: {
+            x: clamp(leftCardBound, rightCardBound, getSnapPos(pos)),
+          },
+          immediate: false,
+          config: {
+            velocity: vx * FPS,
+            ...config.default,
+          },
+        })
+      } else {
+        // User has stopped dragging, determine a target position mimicking inertia
+        // and then snap to the closest card position.
+        // Calculate target displacement using exponential decay function
+        const lambda = 0.3
+        // react-spring uses px/s, while useDrag uses px/frame @60fps
+        const velocity = Math.sign(vx) * Math.max(Math.abs(vx) * FPS, 10) // Set lower bound to prevent singularity and provide "snapping" feel when the user lets go at low velocities
+        // Exponential decay technically takes infinite time. The only way to solve for time
+        // is to bound it by a final velocity > 0
+        // Base eqs:
+        // vf = vi * e^(-lambda * t)
+        // t = -1 / lambda * ln (vf / vi)
+        const targetVelocity = 1 * Math.sign(velocity)
+        const displacement = velocity / lambda
+        const target = pos + displacement
+        const duration = -Math.log(targetVelocity / velocity) / lambda
+        // Exponential decay function found by solving for t and normalizing to f: [0, 1] => [0, 1]
+        // TODO Unsure why I have to multiply by 10, but it makes the initial velocity match up properly
+        const easing = (t: number): number =>
+          (-velocity / (lambda * displacement)) * (Math.exp(-lambda * (t * 10) * duration) - 1)
+
+        setDragXSpring({
+          to: {
+            x: clamp(leftCardBound, rightCardBound, getSnapPos(target)),
+          },
+          immediate: false,
+          config: {
+            velocity,
+            // Convert to ms
+            duration: duration * 1000,
+            easing,
+          },
+        })
+      }
+    },
+    {axis: 'x'}
+  )
 
   // manageContainerWidth
   useLayoutEffect(() => {
@@ -197,14 +233,16 @@ export default function PlayerHand(props: PlayerHandProps): JSX.Element {
         {cards.map((card, cardIndex) => (
           <animated.div
             key={card.id}
+            {...cardDragEvents(cardIndex)}
             style={{
               x: cardIndex * CARD_WIDTH,
               y: cardSprings[cardIndex].y,
-              ...currentCardStyleProps[cardIndex],
             }}
             css={cardCss}
           >
-            <Card text={card.text} type={CardType.White} />
+            <animated.div style={{...currentCardStyleProps[cardIndex]}}>
+              <Card text={card.text} type={CardType.White} />
+            </animated.div>
           </animated.div>
         ))}
       </animated.div>
