a88232c663e36341ebabaa8f37155a227954de8d
Paavan Bhavsar <paavanb@gmail.com>
2020-05-09 13:05:16 -0400

    Rename Game dir to CardsAgainstHumanity

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -2,13 +2,13 @@ import * as React from 'react'
 import {Route, Switch} from 'react-router-dom'
 
 import Homepage from './Homepage'
-import Game from './Game'
+import CardsAgainstHumanity from './CardsAgainstHumanity'
 
 export default function AppRouter(): JSX.Element {
   return (
     <Switch>
       {/* (g)ame */}
-      <Route path="/g" component={Game} />
+      <Route path="/g" component={CardsAgainstHumanity} />
       <Route path="/" component={Homepage} exact />
     </Switch>
   )
