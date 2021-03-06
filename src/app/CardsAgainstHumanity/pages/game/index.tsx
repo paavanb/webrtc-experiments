440c46bfeef1fca8928858ca1675df7788a75dbd
Paavan Bhavsar <paavanb@gmail.com>
2021-03-06 17:35:17 -0500

    Separate app into folders, create GamePage

---
+++ src/app/CardsAgainstHumanity/pages/game/index.tsx
@@ -0,0 +1,134 @@
+import {Button, List, ListItem, ListItemIcon, ListItemText, Typography} from '@material-ui/core'
+
+import {Dictionary} from '../../../../lib/types'
+import {BlackCard, WhiteCard, ClientId, CardId, CompleteRound} from '../../game/types'
+import {getWhiteCard} from '../../data/white-cards-2.1'
+import {getBlackCard} from '../../data/black-cards-2.1'
+
+import PlayerCards from './PlayerCards'
+
+function printCards(cards: (WhiteCard | BlackCard)[]): string {
+  return cards.map(({text}) => text).join(', ')
+}
+
+interface GamePageProps {
+  username: string
+  blackCard: BlackCard | null
+  czar: ClientId | null
+  clientId: ClientId
+  playerHand: WhiteCard[]
+  submissions: Dictionary<ClientId, CardId[]>
+  roundHistory: CompleteRound[]
+  onRequestCzar(): void
+  onSelectWinner(clientId: ClientId): () => void
+  onSubmitCards(cards: WhiteCard[]): void
+}
+
+export default function GamePage(props: GamePageProps): JSX.Element {
+  const {
+    username,
+    blackCard,
+    roundHistory,
+    playerHand,
+    onRequestCzar,
+    onSelectWinner,
+    onSubmitCards,
+    czar,
+    submissions,
+    clientId,
+  } = props
+
+  const submittedCards = submissions[clientId]
+  const hasSubmittedCards = submittedCards !== undefined
+
+  const isPlayingRound = czar != null
+  const isCzar = clientId === czar
+  const isSerf = isPlayingRound && !isCzar
+
+  return (
+    <div>
+      <div>
+        Username: <strong>{username}</strong>.
+      </div>
+      {!blackCard ? (
+        <div>
+          <Button onClick={onRequestCzar} variant="contained" color="secondary">
+            Draw the next black card
+          </Button>
+        </div>
+      ) : (
+        <span>
+          {isCzar ? 'I just drew: ' : 'Currently playing: '}
+          <strong>{blackCard.text}</strong>
+        </span>
+      )}
+      {submittedCards && (
+        <div>
+          You said: <strong>{submittedCards.map((id) => getWhiteCard(id).text).join(', ')}</strong>
+        </div>
+      )}
+      {isCzar && (
+        <div>
+          <Typography variant="h6" component="h1">
+            Submissions
+          </Typography>
+          {Object.keys(submissions).length === 0 ? (
+            'Waiting for submissions...'
+          ) : (
+            <List>
+              {Object.entries(submissions).map((pair, index) => {
+                const [peerClientId, cardIds] = pair
+                if (cardIds === undefined) return undefined
+                return (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <ListItem key={index}>
+                    <ListItemIcon>
+                      <Button
+                        onClick={onSelectWinner(peerClientId)}
+                        variant="contained"
+                        color="primary"
+                      >
+                        Winner
+                      </Button>
+                    </ListItemIcon>
+                    <ListItemText primary={printCards(cardIds.map(getWhiteCard))} />
+                  </ListItem>
+                )
+              })}
+            </List>
+          )}
+        </div>
+      )}
+      <div css={{marginTop: 24}}>
+        <PlayerCards
+          cards={playerHand}
+          onSelectCards={onSubmitCards}
+          cardsToPick={isSerf && !hasSubmittedCards && blackCard ? blackCard.pick : 0}
+        />
+      </div>
+      {roundHistory.length > 0 && (
+        <section>
+          <Typography variant="h6" component="h1">
+            History
+          </Typography>
+          <List>
+            {roundHistory.map((historicalRound, index) => {
+              const roundBlackCard = getBlackCard(historicalRound.blackCard)
+              const cardIds = historicalRound.submissions[historicalRound.winner] as number[]
+              const winnersCards = cardIds.map(getWhiteCard)
+              return (
+                // eslint-disable-next-line react/no-array-index-key
+                <ListItem key={index}>
+                  <ListItemText
+                    primary={roundBlackCard.text}
+                    secondary={winnersCards.map(({text}) => text).join(', ')}
+                  />
+                </ListItem>
+              )
+            })}
+          </List>
+        </section>
+      )}
+    </div>
+  )
+}
--- src/components/WebRTCAppController.tsx
+++ src/components/WebRTCAppController.tsx
@@ -53,6 +53,7 @@ interface WebRTCAppClientProps {
   username: string
   serverPeer: SwarmPeer
   selfMetadata: PeerMetadata
+  peers: SwarmPeer[]
 }
 
 interface WebRTCAppControllerProps {
@@ -84,12 +85,14 @@ export default function WebRTCAppController(props: WebRTCAppControllerProps): JS
   const [pkHash, setPkHash] = useState<string | null>(null)
   const signKeyPair = useSignKeyPair()
 
+  const peers = useMemo(() => Object.values(conns), [conns])
+
   // Ensure that query param state is synced to leader value
   if (isLeader && leader !== pkHash) setLeader(pkHash)
   const swarmCommExtension = useSwarmCommExtension({
     username,
     onPeerAdd: (ext, metadata) => {
-      setConns(prevConns => ({
+      setConns((prevConns) => ({
         ...prevConns,
         [metadata.id]: {
           metadata,
@@ -100,7 +103,7 @@ export default function WebRTCAppController(props: WebRTCAppControllerProps): JS
     signKeyPair,
     onPeerDrop: (_, key) => {
       log('Peer dropped: ', key)
-      setConns(prevConns => omit(prevConns, [key]))
+      setConns((prevConns) => omit(prevConns, [key]))
     },
   })
 
@@ -119,15 +122,15 @@ export default function WebRTCAppController(props: WebRTCAppControllerProps): JS
   const rawClientPeers = useMemo(
     () =>
       [
-        ...Object.values(conns).filter(peer => pkHash !== null && peer.metadata.leader === pkHash),
+        ...peers.filter((peer) => pkHash !== null && peer.metadata.leader === pkHash),
         localhostClientPeer,
       ].filter(isNotEmpty),
-    [pkHash, conns, localhostClientPeer]
+    [peers, pkHash, localhostClientPeer]
   )
 
   const changePeerLeader = useCallback(
     (peer: SwarmPeer, leaderId: string | null) => {
-      setConns(prevConns => ({
+      setConns((prevConns) => ({
         ...prevConns,
         [peer.metadata.id]: {
           ...prevConns[peer.metadata.id],
@@ -186,7 +189,7 @@ export default function WebRTCAppController(props: WebRTCAppControllerProps): JS
   useEffect(() => {
     // Candidates are peers which recognize themselves to be the leader
     const candidateLeaders = Object.values(conns).filter(
-      peer => peer.metadata.leader === peer.metadata.id
+      (peer) => peer.metadata.leader === peer.metadata.id
     )
     if (!isLeader && leader === null && candidateLeaders.length) {
       setLeader(candidateLeaders[0].metadata.id)
@@ -216,6 +219,7 @@ export default function WebRTCAppController(props: WebRTCAppControllerProps): JS
                   username={username}
                   selfMetadata={selfMetadata}
                   serverPeer={localhostServerPeer}
+                  peers={peers}
                 />
               )
             : leaderPeer && (
@@ -223,11 +227,12 @@ export default function WebRTCAppController(props: WebRTCAppControllerProps): JS
                   username={username}
                   selfMetadata={selfMetadata}
                   serverPeer={leaderPeer}
+                  peers={peers}
                 />
               )}
         </div>
       )}
-      {Object.keys(conns).map(hash => (
+      {Object.keys(conns).map((hash) => (
         <ConnectionController key={hash} peer={conns[hash]} onPeerLeaderChange={changePeerLeader} />
       ))}
     </div>
